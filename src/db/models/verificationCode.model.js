e443f104221cf04624583effafffbf79e4671bee
sahilshivekar <sahilshivekarce@gmail.com>
2025-01-24 16:06:04 +0530

    [Model] Add VerificationCode model

---
+++ src/db/models/verificationCode.model.js
@@ -0,0 +1,61 @@
+import { Sequelize, Model } from 'sequelize';
+import sequelize from '../../config/db.connection.js'; // Your sequelize instance
+import Admin from './admin.model.js';
+
+
+class VerificationCode extends Model { }
+
+VerificationCode.init(
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            field: 'verification_code_id'
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: 'Email cannot be empty',
+                },
+                isEmail: {
+                    msg: 'Must be a valid email address',
+                }
+            },
+        },
+        code: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: 'Code cannot be empty'
+                }
+            }
+        },
+        expiresAt: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            field: 'expires_at'
+        },
+        createdAt: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            field: 'created_at'
+        },
+        updatedAt: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            field: 'updated_at'
+        },
+    },
+    {
+        sequelize,
+        modelName: 'VerificationCode',
+        tableName: 'verification_codes', // Specifies the actual table name in the database
+    });
+
+VerificationCode.belongsTo(Admin, { foreignKey: 'email', targetKey: 'email' });
+
+export default VerificationCode;
