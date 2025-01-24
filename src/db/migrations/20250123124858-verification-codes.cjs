ddd820693916d48bc4f956bf201db58a1140ae6c
sahilshivekar <sahilshivekarce@gmail.com>
2025-01-24 15:59:37 +0530

    [Migration] Create verification_codes table

---
+++ src/db/migrations/20250123124858-verification-codes.cjs
@@ -0,0 +1,65 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable(
+            'verification_codes',
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    field: 'verification_code_id'
+                },
+                email: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    references: {
+                        model: 'admins',
+                        key: 'admin_email'
+                    },
+                    validate: {
+                        notEmpty: {
+                            msg: 'Email cannot be empty',
+                        },
+                        isEmail: {
+                            msg: 'Must be a valid email address',
+                        }
+                    },
+                },
+                code: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    validate: {
+                        notEmpty: {
+                            msg: 'Code cannot be empty'
+                        }
+                    }
+                },
+                expiresAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    field: 'expires_at'
+                },
+                createdAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    field: 'created_at'
+                },
+                updatedAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    field: 'updated_at'
+                },
+            },
+            {
+                freezeTableName: true,
+            }
+        )
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('verification_codes')
+    }
+};
