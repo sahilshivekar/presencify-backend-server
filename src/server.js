f518da6f6382b0b08aab9b703bd8a49c8e88dd1a
sahilshivekar <sahilshivekarce@gmail.com>
2025-02-12 15:19:09 +0530

    Resolving the self ping issue

--- src/server.js
+++ src/server.js
@@ -8,12 +8,13 @@ const port = process.env.PORT || 8000;
 
 // for stoping the server instance on render to go down with inactivity
 app.get("/api/v1/self-ping", async (req, res) => {
-    res.send(null);
+    res.status(200).send(null);
 })
 
+console.log(process.env.SERVER_ADDRESS + "/api/v1/self-ping")
 setInterval(() => {
     https.get(process.env.SERVER_ADDRESS + "/api/v1/self-ping", (res) => {
-        console.log("Self-ping: ", res)
+        console.log("Self-ping: ", res.statusCode)
     }).on('error', (err) => {
         console.log("Error during self-ping: ", err)
     })
