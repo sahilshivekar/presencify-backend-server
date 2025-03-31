04acd1f00d5c7191c733d47d9428672cc63e2348
sahilshivekar <sahilshivekarce@gmail.com>
2025-03-31 16:04:11 +0530

    Add admin only access to batch routes

--- src/routes/batch.routes.js
+++ src/routes/batch.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import { verifyAdminJWT } from "../middlewares/auth.middleware.js"
+
 import { getBatches, addBatch, updateBatch, removeBatch, getBatchById } from '../controllers/batch.controller.js';
 
 const router = express.Router();
 
-router.route('/get-batches').get(getBatches);
-router.route('/add').post(addBatch);
-router.route('/update').put(updateBatch);
-router.route('/remove').delete(removeBatch);
-router.route('/get-batch-by-id').get(getBatchById);
+router.route('/get-batches').get(verifyAdminJWT, getBatches);
+router.route('/add').post(verifyAdminJWT, addBatch);
+router.route('/update').put(verifyAdminJWT, updateBatch);
+router.route('/remove').delete(verifyAdminJWT, removeBatch);
+router.route('/get-batch-by-id').get(verifyAdminJWT, getBatchById);
 export default router;
