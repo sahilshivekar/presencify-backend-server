b350a2383a082a8754afd56502a59314525a0a1e
sahilshivekar <sahilshivekarce@gmail.com>
2025-05-31 13:47:26 +0530

    Commentout the self ping and allow all the devices in same network to send request by setting in the app.listen() method 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -7,19 +7,19 @@ const port = process.env.PORT || 8000;
 
 
 // for stoping the server instance on render to go down with inactivity
-app.get("/api/v1/self-ping", async (req, res) => {
-    res.status(200).send(null);
-})
+// app.get("/api/v1/self-ping", async (req, res) => {
+//     res.status(200).send(null);
+// })
 
-setInterval(() => {
-    https.get(process.env.SERVER_ADDRESS + "/api/v1/self-ping", (res) => {
-        console.log("Self-ping: ", res.statusCode)
-    }).on('error', (err) => {
-        console.log("Error during self-ping: ", err.message)
-    })
-}, 1000 * 60 * 3) // 3 minutes
+// setInterval(() => {
+//     https.get(process.env.SERVER_ADDRESS + "/api/v1/self-ping", (res) => {
+//         console.log("Self-ping: ", res.statusCode)
+//     }).on('error', (err) => {
+//         console.log("Error during self-ping: ", err.message)
+//     })
+// }, 1000 * 60 * 3) // 3 minutes
 
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running at port: ${port}`);
 });
