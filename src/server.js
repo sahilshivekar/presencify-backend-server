2af9548eb299df845a3cdf6fbf1019d43ca7956b
Sahl Shivekar <147322755+sahilshivekar@users.noreply.github.com>
2025-09-25 16:06:55 +0530

    Update default port from 8000 to 4000 in server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import app from "./app.js"
 import { config } from './config/config.js'
 import { logger } from './config/logger.js';
 
-const port = config.port || 8000;
+const port = config.port || 4000;
 
 app.listen(port, '0.0.0.0', () => {
     logger.info(`Server started on port ${port} [env: ${config.env}]`);
